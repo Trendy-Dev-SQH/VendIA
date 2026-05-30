1ee50d36dc0df069102883b8b1f77bca2ad7cc18
Trendy-Dev-SQH <juansb230502@gmail.com>
2026-05-30 14:37:43 -0500

    Add forceRedirectUrl prop to SignUp component

--- frontend/app/sign-up/[[...sign-up]]/page.tsx
+++ frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function SignUpPage() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-50">
-      <SignUp />
+      <SignUp forceRedirectUrl="/dashboard" />
     </div>
   )
 }
